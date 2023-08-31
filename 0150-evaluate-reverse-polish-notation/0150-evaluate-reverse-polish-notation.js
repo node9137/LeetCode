c092821227f7e9c156f95c43a8806f5d6d67e69f
node9137 <139099596+node9137@users.noreply.github.com>
2023-09-01 00:53:37 +0900

    Time: 58 ms (82.80%), Space: 44.9 MB (69.78%) - LeetHub

---
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+var evalRPN = function(tokens) {
+    let stack = [];
+    for (let i = 0; i < tokens.length; i++) {
+        if (tokens[i] == '+') {
+            stack.push(stack.pop() + stack.pop());
+        } else if (tokens[i] == '-') {
+            stack.push(- stack.pop() + stack.pop());
+        } else if (tokens[i] == '*') {
+            stack.push(stack.pop() * stack.pop());
+        } else if (tokens[i] == '/') {
+            stack.push(Math.trunc(1/(stack.pop()/stack.pop())));
+        } else stack.push(parseInt(tokens[i]));
+    };
+    return stack[0];
+
+}
